c8ecad5dce74db5ad455d0c021aafe242b76f823
MordoJ <35224441+MordoJ@users.noreply.github.com>
2022-03-13 19:56:58 +0100

    Adding clean command

--- src/main/java/ch/heig/dil/Starter.java
+++ src/main/java/ch/heig/dil/Starter.java
@@ -6,7 +6,7 @@ import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
 
 @Command(name = "starter", version = "starter 1.0",
-         subcommands = {New.class, Build.class})
+         subcommands = {New.class, Build.class, Clean.class})
 public class Starter implements Callable<Integer>{
     public static void main(String... args) {
         int exitCode = new CommandLine(new Starter()).execute(args);
