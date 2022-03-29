0b3b52a6825306382e05d80af976ec63cc8696a1
Clement Dieperink <clement.dieperink@heig-vd.ch>
2022-03-29 11:37:50 +0200

    implement deleteRecursive using FileUtils from commons-io

--- src/main/java/ch/heigvd/statique/commands/Utils.java
+++ src/main/java/ch/heigvd/statique/commands/Utils.java
@@ -1,12 +1,13 @@
 package ch.heigvd.statique.commands;
 
+import org.apache.commons.io.FileUtils;
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class Utils {
 
   public static void deleteRecursive(Path directory) throws IOException {
-    throw new UnsupportedOperationException();
+    FileUtils.deleteDirectory(directory.toFile());
   }
 
 }
