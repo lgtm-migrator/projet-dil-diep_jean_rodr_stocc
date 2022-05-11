d28ef645f80b5d3ec4f8c1396e71df2c06dd16c2
MordoJ <35224441+MordoJ@users.noreply.github.com>
2022-05-11 15:21:55 +0200

    Putting back the unproblematic tests

---
+++ src/test/java/ch/heigvd/statique/commands/VersionTest.java
@@ -0,0 +1,21 @@
+package ch.heigvd.statique.commands;
+import ch.heigvd.statique.Statique;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VersionTest {
+
+    @Test
+    void show() throws IOException {
+        try (ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            System.setOut(new PrintStream(output));
+            new CommandLine(new Statique()).execute("--version");
+            assertTrue((output.toString().contains("Statique Version v0.0.1")));
+        }
+    }
+
+}
